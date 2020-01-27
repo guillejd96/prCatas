11f86f7ffcdd2cd8d369e00d868380e1e04e86f5
Guillermo <wasanga7@gmail.com>
2020-01-27 22:09:56 +0100

    changes

--- Inicio.php
+++ Inicio.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Inicio</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/init.css">
 </head>
