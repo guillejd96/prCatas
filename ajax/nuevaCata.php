16e5eede0f16fd85125bfd670c60a0fcbfd6f40b
Guillermo <wasanga7@gmail.com>
2020-02-14 11:31:48 +0100

    Changes to upload correctly the cata data

--- ajax/nuevaCata.php
+++ ajax/nuevaCata.php
@@ -2,12 +2,12 @@
 
 include '../config.php';
 
-$nombre = $_GET['nombre'];
+$nombre = $_GET['cata'];
 $fecha = date("Y-m-d",strtotime($_GET['fecha']));
 
 $sql = "INSERT INTO cata (nombre,fecha) VALUES ('".$nombre."',CAST('".$fecha."' AS DATE));";
 if(mysqli_query($conexion,$sql)){
-	$sql = "SELECT id FROM cata WHERE nombre='".$nombre."' AND fecha=".$fecha.";";
+	$sql = "SELECT id FROM cata WHERE nombre='".$nombre."' AND fecha=CAST('".$fecha."' AS DATE);";
 	$id = mysqli_query($conexion,$sql)->fetch_row()[0];
 	echo $id;
 }else {
