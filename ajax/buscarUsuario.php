fa2bf82d52214b93e1df0b538f32d9508209f681
Guillermo <wasanga7@gmail.com>
2020-04-07 18:44:26 +0200

    Create buscarUsuario.php

---
+++ ajax/buscarUsuario.php
@@ -0,0 +1,18 @@
+<?php 
+
+include '../config.php';
+
+$u = $_GET['u'];
+$p = $_GET['p'];
+
+$sql = "SELECT id FROM persona WHERE usuario='".$u."' AND password='".$p."';";
+
+$res = mysqli_query($conexion,$sql);
+
+if(mysqli_num_rows($res)>0){
+	echo $res->fetch_row()[0];
+}else{
+	echo "error";
+}
+ 
+?>
