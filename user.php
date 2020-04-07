3deece9f3bb8f51f277a05e77b81198fb4a8a4cf
Guillermo <wasanga7@gmail.com>
2020-04-07 18:45:11 +0200

    Create user.php

---
+++ user.php
@@ -0,0 +1,18 @@
+
+<?php include "config.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- AQUI FALTA GUARDAR EL ID DE USUARIO QUE VIENE POR GET -->
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+	<h1>Servidor de catas</h1><br>
+	<button type="button" class="btn btn-dark" onclick="location.href = 'nueva_cata.php'">Nueva cata</button> <br><br>
+	<button type="button" class="btn btn-dark" onclick="location.href = 'cata.php'">Ver catas</button> <br>
+</body>
+</html> 
