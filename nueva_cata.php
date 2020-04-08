4b9a0a13d24b2261c28edf6f823e878ed6626653
Guillermo <wasanga7@gmail.com>
2020-04-08 19:43:59 +0200

    Update nueva_cata.php

--- nueva_cata.php
+++ nueva_cata.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<title>Nueva cata</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/init.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script>
 		var personas,cervezas;
@@ -111,50 +111,9 @@
 		<tr><td colspan="2"><br></td></tr>
 		<tr>
 			<td><button class="btn btn-secondary" id="continuar">Continuar</button></td>
-			<td><button class="btn btn-link" onclick="location.href='index.php'">Volver</button></td>
+			<td><button class="btn btn-link" onclick="location.href='user.php'">Volver</button></td>
 		</tr>
 	</table>
 	<br><br>
-
-	<!--
-	
-	<form id="nueva_cata">
-
-	<div class="form-group">
-    <label for="exampleFormControlTextarea1">Nombre de la cata</label>
-    <textarea class="form-control"rows="1" title="Un nombre para registrar la nueva cata" class="entrada" style="width: 45%; margin-left: 27.5%;"></textarea>
- </div>
- <div class="form-group">
-    <label for="exampleFormControlSelect1">Número de borrachos</label>
-    <select class="form-control"  class="entrada" style="width: 45%; margin-left: 27.5%;">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
-      <option>10</option>
-      <option>11</option>
-      <option>12</option>
-      <option>13</option>
-      <option>14</option>
-      <option>15</option>
-      <option>16</option>
-    </select>
-</div>
- 	<div class="form-group">
-    <label for="exampleFormControlTextarea1">Nombre de las cervezas</label>
-    <textarea class="form-control" rows="10" title="Separar cada cerveza mediante una coma (una, otra, otra)" class="entrada" style="width: 45%; margin-left: 27.5%;"></textarea>
-</div>
-<div>
-	<label for="exampleFormControlTextarea1">Elige la fecha de la cata</label> <br>
-	<input type="date" id="cervezas" style="width: 25%;">
-</div>
-</form>
-
--->
 </body>
 </html>
