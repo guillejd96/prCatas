27c79e05ac5247114bb01a2465f2189e0f3c7b37
Guillermo <wasanga7@gmail.com>
2020-04-12 19:51:12 +0200

    Update cata.php

--- cata.php
+++ cata.php
@@ -1,7 +1,20 @@
 <?php 
 	include "config.php";
 
-	$resCatas = mysqli_query($conexion,"SELECT * FROM cata");
+	session_start();
+	if(!isset($_SESSION["idUsuario"])){
+		header('Location: index.php');
+	}
+
+	$idUsuario = $_SESSION["idUsuario"];
+
+	$sqlPersona = "SELECT * FROM persona WHERE idUsuario=".$idUsuario;
+	$resPersona = mysqli_query($conexion,$sqlPersona)->fetch_row();
+
+	$idPersona = $resPersona[0];
+
+	$sqlPersonaCata = "SELECT idCata FROM persona_cata WHERE idPersona = ".$idPersona;
+	$resIDCatas = mysqli_query($conexion,$sqlPersonaCata);
  ?>
 
  <!DOCTYPE html>
@@ -15,7 +28,8 @@
  </head>
  <body>
  	<h1>Catas guardadas</h1><br>
- 	<?php if($resCatas->num_rows>0){
+ 	<h3>Aquí se muestran un listado de las catas que has registrado</h3>
+ 	<?php if($resIDCatas->num_rows>0){
  		echo "<table>";
  		echo "<tr>";
  		echo "<th><p>ID</p></th>";
@@ -24,25 +38,35 @@
  		echo "<th><p>Personas</p></th>";
  		echo "<th><p>Cervezas</p></th>";
  		echo "</tr>";
-		while ($cata = mysqli_fetch_assoc($resCatas)) {
-			$idCata = $cata['id'];
-			$resCervezas = mysqli_query($conexion,"SELECT COUNT(*) FROM cerveza WHERE idCata = $idCata");
+		while ($idCata = mysqli_fetch_array($resIDCatas)) {
+			$sqlCata = "SELECT * FROM cata WHERE id=".$idCata[0];
+			$cata = mysqli_query($conexion,$sqlCata)->fetch_row();
+			$resCervezas = mysqli_query($conexion,"SELECT COUNT(*) FROM cerveza WHERE idCata = ".$idCata[0]);
 			$nCervezas = $resCervezas->fetch_row()[0];
-			$resPersonas = mysqli_query($conexion,"SELECT COUNT(*) FROM persona WHERE idCata = $idCata");
+			$resPersonas = mysqli_query($conexion,"SELECT COUNT(*) FROM persona WHERE idCata = ".$idCata[0]);
 			$nPersonas = $resPersonas->fetch_row()[0];
 			echo "<tr>";
 			echo "<td><p><a href='info_cata.php?id=".$idCata."'>".$idCata."</a></p>";
-			echo "<td><p>".$cata['nombre']."</p></td>";
-			echo "<td><p>".$cata['fecha']."</p></td>";
+			echo "<td><p>".$cata[1]."</p></td>";
+			echo "<td><p>".$cata[2]."</p></td>";
 			echo "<td><p>".$nCervezas."</p></td>";
 			echo "<td><p>".$nPersonas."</p></td>";
 			echo "</tr>";
 		}
 		echo "</table>";
  	} else { ?>
-		<p>No hay catas en la base de datos</p>
+ 		<table>
+ 			<tr>
+ 				<td><p>No has registrado ninguna cata</p></td>
+ 			</tr>
+ 			<tr>
+ 				<td><p>Para registrar tu primera cata pulsa <a href="nueva_cata.php">aquí</a></p></td>
+ 			</tr>
+ 		</table>
+		
+		
  	<?php } ?>
  	<br>
- 	<button class="btn btn-dark" onclick="location.href='user.php'">Volver</button>
+ 	<button class="btn btn-link" onclick="location.href='user.php'">Volver</button>
  </body>
  </html>
