feac179bcfc43bd4d0ab6b977f2408addec9682c
Guillermo <wasanga7@gmail.com>
2020-04-12 19:51:05 +0200

    Update nuevaCata.php

--- ajax/nuevaCata.php
+++ ajax/nuevaCata.php
@@ -2,14 +2,21 @@
 
 include '../config.php';
 
-$nombre = $_GET['cata'];
-$fecha = date("Y-m-d",strtotime($_GET['fecha']));
+$nombre = $_GET['c'];
+$idPersona = $_GET['p'];
+$fecha = date("Y-m-d",strtotime($_GET['f']));
 
 $sql = "INSERT INTO cata (nombre,fecha) VALUES ('".$nombre."',CAST('".$fecha."' AS DATE));";
 if(mysqli_query($conexion,$sql)){
 	$sql = "SELECT id FROM cata WHERE nombre='".$nombre."' AND fecha=CAST('".$fecha."' AS DATE);";
 	$id = mysqli_query($conexion,$sql)->fetch_row()[0];
-	echo $id;
+
+	$sqlPersonaCata = "INSERT INTO persona_cata(idPersona,idCata) VALUES (".$idPersona.",".$id.")";
+	if(mysqli_query($conexion,$sqlPersonaCata)){
+		echo $id;
+	}else {
+		echo "-1";
+	}
 }else {
 	echo "0";
 }
