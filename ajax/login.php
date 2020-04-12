8a50d735bbe97921eccd689948cf937f7dc0d933
Guillermo <wasanga7@gmail.com>
2020-04-12 19:51:03 +0200

    Update login.php

--- ajax/login.php
+++ ajax/login.php
@@ -6,8 +6,12 @@ session_start();
 
 $u = $_GET['u'];
 
+$sql = "SELECT id FROM persona WHERE idUsuario=".$u;
+
+$u = mysqli_query($conexion,$sql)->fetch_row()[0];
+
 $_SESSION["idUsuario"] = $u;
 
 echo "1";
- 
+
 ?>
