cd9d78632897a1cf83b9fe57ef30a2a00c08273f
Guillermo <wasanga7@gmail.com>
2020-04-12 19:51:10 +0200

    Update nuevoUsuario.php

--- ajax/nuevoUsuario.php
+++ ajax/nuevoUsuario.php
@@ -6,11 +6,17 @@
 	$nombre = $_GET['n'];
 	$password = $_GET['p'];
 
-	$sql = "INSERT INTO persona(usuario,nombre,password) VALUES ('".$usuario."','".$nombre."','".$password."');";
-
-	if(mysqli_query($conexion,$sql)){
-		echo "1";
-	} else {
-		echo "0";
+	$sqlUsuario = "INSERT INTO usuario(usuario,password) VALUES ('".$usuario."','".$password."');";
+	if(mysqli_query($conexion,$sqlUsuario)){
+		$sqlIDUsuario = "SELECT id FROM usuario WHERE usuario = '".$usuario."' AND password = '".$password."';";
+		$id = mysqli_query($conexion,$sqlIDUsuario)->fetch_row()[0];
+		$sql = "INSERT INTO persona(nombre,idUsuario) VALUES ('".$nombre."',".$id.");";
+		if(mysqli_query($conexion,$sql)){
+			echo "1";
+		} else {
+			echo "0";
+		}
+	}else {
+		echo "-1";
 	}
  ?>
