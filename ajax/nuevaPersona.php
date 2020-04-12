01195df4bc988cb7d1dfc58aeb5ea6b8d5e3fd0e
Guillermo <wasanga7@gmail.com>
2020-04-12 19:51:08 +0200

    Update nuevaPersona.php

--- ajax/nuevaPersona.php
+++ ajax/nuevaPersona.php
@@ -5,11 +5,17 @@ include '../config.php';
 $nombre = $_GET['nombre'];
 $id = $_GET['cata'];
 
-$sql = "INSERT INTO persona (nombre,idCata) VALUES ('".$nombre."',".$id.");";
+$sql = "INSERT INTO persona (nombre) VALUES ('".$nombre."');";
 if(mysqli_query($conexion,$sql)){
-	$sql = "SELECT id FROM persona WHERE nombre='".$nombre."' AND idCata=".$id.";";
-	$id = mysqli_query($conexion,$sql)->fetch_row()[0];
-	echo $id;
+	$sql = "SELECT id FROM persona WHERE nombre='".$nombre."' ORDER BY id DESC;";
+	$idPersona = mysqli_query($conexion,$sql)->fetch_row()[0];
+
+	$sqlPersonaCata = "INSERT INTO persona_cata(idPersona,idCata) VALUES (".$idPersona.",".$id.")";
+	if(mysqli_query($conexion,$sqlPersonaCata)){
+		echo $idPersona;
+	}else {
+		echo "0";
+	}
 }else {
 	echo "0";
 }
