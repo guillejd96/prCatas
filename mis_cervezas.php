41271f318e773821442915196f9c3da6d4e604c2
Guillermo <wasanga7@gmail.com>
2020-04-20 21:16:48 +0200

    Update mis_cervezas.php

--- mis_cervezas.php
+++ mis_cervezas.php
@@ -7,8 +7,13 @@
 
 	$idUsuario = $_SESSION["idUsuario"];
 
-	$sqlCervezas = "SELECT * FROM opinion WHERE idPersona=".$idUsuario;
-	$resCervezas = mysqli_query($conexion,$sqlCervezas);
+	$sqlPersona = "SELECT * FROM persona WHERE idUsuario=".$idUsuario;
+	$resPersona = mysqli_query($conexion,$sqlPersona)->fetch_row();
+
+	$idPersona = $resPersona[0];
+
+	$sql = "SELECT * FROM opinion WHERE idPersona=".$idPersona;
+	$resOpiniones = mysqli_query($conexion,$sql);
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +29,56 @@
 	<h3>Aquí se muestran un listado de todas las valoraciones que has hecho</h3>
 	
 	<table>
-		<?php if(mysqli_num_rows($resCervezas)>0){
+		<?php if(mysqli_num_rows($resOpiniones)>0){
+			echo "<tr>
+		 			<th>
+		 				<p>Cerveza</p>
+		 			</th>
+		 			<th>
+		 				<p>Aroma</p>
+		 			</th>
+		 			<th>
+		 				<p>Apariencia</p>
+		 			</th>
+		 			<th>
+		 				<p>Sabor</p>
+		 			</th>
+		 			<th>
+		 				<p>Cuerpo</p>
+		 			</th>
+		 			<th>
+		 				<p>Botellín</p>
+		 			</th>
+		 			<th>
+		 				<p>Media</p>
+		 			</th>
+		 		</tr>";
+		 		if($resOpiniones->num_rows>0){
+
+ 				while($row = mysqli_fetch_assoc($resOpiniones)){
+ 						$sqlCerveza = "SELECT nombre FROM cerveza WHERE id=".$row['idCerveza'].";";
+	 					$cerveza = mysqli_query($conexion,$sqlCerveza)->fetch_row()[0];
+
+	 					echo "<tr>";
+	 					echo "<td><p>".$cerveza."</p></td>";
+	 					echo "<td><p>".$row['aroma']."</p></td>";
+	 					echo "<td><p>".$row['apariencia']."</p></td>";
+	 					echo "<td><p>".$row['sabor']."</p></td>";
+	 					echo "<td><p>".$row['cuerpo']."</p></td>";
+	 					if($row['botellin']!=NULL){
+	 						echo "<td><p>".$row['botellin']."</p></td>";
+	 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
+	 						echo "<td><p>".$media."</p></td>";
+	 					} else {
+	 						echo "<td><p>--</p></td>";
+	 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']) / 4;
+	 						echo "<td><p>".$media."</p></td>";
+	 					}
+	 					echo "<tr>";
+ 				}
+ 			} else {
+ 				echo "<tr><td colspan='6'><p>No hay resultados disponibles</p></td></tr>";
+ 			}
 
 		} else {
 			echo "<tr><td>";
