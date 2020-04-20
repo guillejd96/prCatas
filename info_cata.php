97185bc6d6e917396b4cb502320289dcc1f3923f
Guillermo <wasanga7@gmail.com>
2020-04-20 21:16:44 +0200

    Update info_cata.php

--- info_cata.php
+++ info_cata.php
@@ -1,12 +1,18 @@
 <?php 
 	include "config.php";
+
+	session_start();
+	if(!isset($_SESSION["idUsuario"])){
+		header('Location: index.php');
+	}
+
 	$idCata = $_GET["id"];
 
 	$sql = "SELECT nombre FROM cata WHERE id=".$idCata;
 	$nombreCata = mysqli_query($conexion,$sql)->fetch_row()[0];
 
-	$sql = "SELECT id,nombre FROM persona WHERE idCata =".$idCata;
-	$resPersonas = mysqli_query($conexion,$sql);
+	$sql = "SELECT idPersona FROM persona_cata WHERE idCata =".$idCata;
+	$resIDPersonas = mysqli_query($conexion,$sql);
 
 	$sql = "SELECT id,nombre FROM cerveza WHERE idCata =".$idCata;
 	$resCerveza = mysqli_query($conexion,$sql);
@@ -18,7 +24,7 @@
 	<meta charset="UTF-8">
 	<title><?php echo $nombreCata ?></title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/init.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
 </head>
 <body>
@@ -26,22 +32,24 @@
 	<table>
 		<tr>
 			<td>
-				<table style='background-color: #FFA900;' border="1">
+				<table class="no_border">
 					<tr>
 						<th>
 							<p>Personas</p>
 						</th>
 					</tr>
 					<?php 
-						while($row = mysqli_fetch_array($resPersonas)){
-							echo "<tr><td scope='row' width='7rem'><a href='persona.php?id=".$row[0]."'><p>".$row[1]."</p></td></tr>";
+						while($row = mysqli_fetch_array($resIDPersonas)){
+							$sqlPersonas = "SELECT id,nombre FROM persona WHERE id=".$row[0];
+							$persona = mysqli_query($conexion,$sqlPersonas)->fetch_row();
+							echo "<tr><td scope='row' width='7rem'><a href='persona.php?id=".$persona[0]."&c=".$idCata."'><p>".$persona[1]."</p></td></tr>";
 						}
 				 	?>
 				</table>
 			</td>
 			<td></td>
 			<td>
-				<table style='background-color: #FFA900;' border="1">
+				<table class="no_border">
 					<tr>
 						<th>
 							<p>Cervezas</p>
@@ -55,10 +63,11 @@
 				</table>
 			</td>
 		</tr>
+		<tr><td colspan="4"><br>
+			<button class="btn btn-dark" onclick="location.href='resultados.php'">Ver resultados</button>
+		</td></tr>
 	</table>
 	<br>
-	<button class="btn btn-dark" onclick="location.href='resultados.php'">Ver resultados</button>
-	<br><br>
-	<button class="btn btn-dark" onclick="location.href='cata.php'">Volver</button></td>
+	<button class="btn btn-link" onclick="location.href='cata.php'">Volver</button></td>
 </body>
 </html>
