b479972deb9518e1d03f975fcbb9c806275c37c8
Guillermo <wasanga7@gmail.com>
2020-04-20 21:16:38 +0200

    Update nuevaOpinion.php

--- ajax/nuevaOpinion.php
+++ ajax/nuevaOpinion.php
@@ -9,19 +9,11 @@ $apariencia = $_GET['ap'];
 $sabor = $_GET['s'];
 $cuerpo = $_GET['cu'];
 $botellin = $_GET['b'];
-if($botellin!=''){
-	$sql = "INSERT INTO opinion(idCerveza, idPersona, apariencia, aroma, sabor, cuerpo, botellin) VALUES (".$cerveza.",".$persona.",".$apariencia.",".$aroma.",".$sabor.",".$cuerpo.",".$botellin.");";
-	if(mysqli_query($conexion,$sql)){
-		echo "1";
-	}else {
-		echo "0";
-	}
-} else { // SIN BOTELLIN
-	$sql = "INSERT INTO opinion (idCerveza, idPersona, apariencia, aroma, sabor, cuerpo) VALUES (".$cerveza.",".$persona.",".$apariencia.",".$aroma.",".$sabor.",".$cuerpo.")";
-	if(mysqli_query($conexion,$sql)){
-		echo "1";
-	}else {
-		echo "0";
-	}
+
+$sql = "INSERT INTO opinion(idCerveza, idPersona, apariencia, aroma, sabor, cuerpo, botellin) VALUES (".$cerveza.",".$persona.",".$apariencia.",".$aroma.",".$sabor.",".$cuerpo.",".$botellin.");";
+if(mysqli_query($conexion,$sql)){
+	echo "1";
+}else {
+	echo "0";
 }
  ?>
