a53f9276ccba37b4236e709eebd5315ebfcedb53
Guillermo <wasanga7@gmail.com>
2020-04-20 21:16:40 +0200

    Update cata.php

--- cata.php
+++ cata.php
@@ -25,6 +25,22 @@
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script>
+		function borrar(id){
+			b = confirm("¿Quiere borrar la cata con ID "+id+"?");
+			if(b){
+				$.get('ajax/borrarCata.php?id='+id, function(data) {
+					if(data=="1"){
+						setTimeout(function () {
+	       						window.location.reload();	
+	    				},1000);
+					}
+				});
+			}
+		}
+	</script>
  </head>
  <body>
  	<h1>Catas guardadas</h1><br>
@@ -37,20 +53,22 @@
  		echo "<th><p>Fecha</p></th>";
  		echo "<th><p>Personas</p></th>";
  		echo "<th><p>Cervezas</p></th>";
+ 		echo "<th><p></p></th>";
  		echo "</tr>";
 		while ($idCata = mysqli_fetch_array($resIDCatas)) {
 			$sqlCata = "SELECT * FROM cata WHERE id=".$idCata[0];
 			$cata = mysqli_query($conexion,$sqlCata)->fetch_row();
 			$resCervezas = mysqli_query($conexion,"SELECT COUNT(*) FROM cerveza WHERE idCata = ".$idCata[0]);
 			$nCervezas = $resCervezas->fetch_row()[0];
-			$resPersonas = mysqli_query($conexion,"SELECT COUNT(*) FROM persona WHERE idCata = ".$idCata[0]);
+			$resPersonas = mysqli_query($conexion,"SELECT COUNT(*) FROM persona_cata WHERE idCata = ".$idCata[0]);
 			$nPersonas = $resPersonas->fetch_row()[0];
 			echo "<tr>";
-			echo "<td><p><a href='info_cata.php?id=".$idCata."'>".$idCata."</a></p>";
+			echo "<td><p><a href='info_cata.php?id=".$idCata[0]."'>".$idCata[0]."</a></p>";
 			echo "<td><p>".$cata[1]."</p></td>";
 			echo "<td><p>".$cata[2]."</p></td>";
 			echo "<td><p>".$nCervezas."</p></td>";
 			echo "<td><p>".$nPersonas."</p></td>";
+			echo "<td><p><button class='btn btn-link' onclick='javascript:borrar($idCata[0])'><i class='far fa-trash-alt' style='font-size: 25px'></i></button> <button class='btn btn-link'><i class='far fa-edit' style='font-size: 25px'></i></button></p></td>";
 			echo "</tr>";
 		}
 		echo "</table>";
@@ -63,7 +81,6 @@
  				<td><p>Para registrar tu primera cata pulsa <a href="nueva_cata.php">aquí</a></p></td>
  			</tr>
  		</table>
-		
 		
  	<?php } ?>
  	<br>
