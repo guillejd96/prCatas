555496eac4e578532dc5b1a769146649803aeeaa
Guillermo <wasanga7@gmail.com>
2020-04-20 21:16:52 +0200

    Update persona.php

--- persona.php
+++ persona.php
@@ -1,9 +1,17 @@
 <?php 
 	include "config.php";
+
+	session_start();
+	if(!isset($_SESSION["idUsuario"])){
+		header('Location: index.php');
+	}
+
 	$id = $_GET["id"];
 
-	$sql = "SELECT nombre,idCata FROM persona WHERE id=".$id;
-	$persona = mysqli_query($conexion,$sql)->fetch_row();
+	$idCata = $_GET["c"];
+
+	$sql = "SELECT nombre FROM persona WHERE id=".$id;
+	$nombrePersona = mysqli_query($conexion,$sql)->fetch_row()[0];
 
 
 	$sql = "SELECT * FROM opinion WHERE idPersona=".$id;
@@ -14,15 +22,15 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
- 	<title><?php echo $persona[1]; ?></title>
+ 	<title><?php echo $nombrePersona; ?></title>
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/init.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
  </head>
  <body>
- 	<h1>Información de <?php echo $persona[0]; ?></h1>
+ 	<h1>Información de <?php echo $nombrePersona; ?></h1>
  	<br>
- 	<table style='background-color: #FFA900;' border="1">
+ 	<table>
  		<tr>
  			<th>
  				<p>Cerveza</p>
@@ -48,26 +56,36 @@
  		</tr>
  		<?php 
  			if($resOpiniones->num_rows>0){
- 				while($row = mysqli_fetch_assoc($resOpiniones)){
- 					$sqlCerveza = "SELECT nombre FROM cerveza WHERE id=".$row['idCerveza'].";";
- 					$cerveza = mysqli_query($conexion,$sqlCerveza)->fetch_row()[0];
 
- 					echo "<tr>";
- 					echo "<td><p>".$cerveza."</p></td>";
- 					echo "<td><p>".$row['aroma']."</p></td>";
- 					echo "<td><p>".$row['apariencia']."</p></td>";
- 					echo "<td><p>".$row['sabor']."</p></td>";
- 					echo "<td><p>".$row['cuerpo']."</p></td>";
- 					if($row['botellin']!=NULL){
- 						echo "<td><p>".$row['botellin']."</p></td>";
- 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
- 						echo "<td><p>".$media."</p></td>";
- 					} else {
- 						echo "<td><p>--</p></td>";
- 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']) / 4;
- 						echo "<td><p>".$media."</p></td>";
+ 				$sqlCervezas = "SELECT id FROM cerveza WHERE idCata=".$idCata;
+ 				$resCervezas = mysqli_query($conexion,$sqlCervezas);
+ 				$idCervezas = array();
+ 				while($row = mysqli_fetch_array($resCervezas)){
+ 					array_push($idCervezas, $row[0]);
+ 				}
+
+ 				while($row = mysqli_fetch_assoc($resOpiniones)){
+ 					if(in_array($row['idCerveza'], $idCervezas)){
+ 						$sqlCerveza = "SELECT nombre FROM cerveza WHERE id=".$row['idCerveza'].";";
+	 					$cerveza = mysqli_query($conexion,$sqlCerveza)->fetch_row()[0];
+
+	 					echo "<tr>";
+	 					echo "<td><p>".$cerveza."</p></td>";
+	 					echo "<td><p>".$row['aroma']."</p></td>";
+	 					echo "<td><p>".$row['apariencia']."</p></td>";
+	 					echo "<td><p>".$row['sabor']."</p></td>";
+	 					echo "<td><p>".$row['cuerpo']."</p></td>";
+	 					if($row['botellin']!=NULL){
+	 						echo "<td><p>".$row['botellin']."</p></td>";
+	 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
+	 						echo "<td><p>".$media."</p></td>";
+	 					} else {
+	 						echo "<td><p>--</p></td>";
+	 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']) / 4;
+	 						echo "<td><p>".$media."</p></td>";
+	 					}
+	 					echo "<tr>";
  					}
- 					echo "<tr>";
  				}
  			} else {
  				echo "<tr><td colspan='6'><p>No hay resultados disponibles</p></td></tr>";
@@ -75,6 +93,6 @@
  		 ?>
  	</table>
  	<br>
- 	<button class="btn btn-dark" onclick="location.href='info_cata.php?id=<?php echo $persona[1]; ?>'">Volver</button>
+ 	<button class="btn btn-link" onclick="location.href='info_cata.php?id=<?php echo $idCata; ?>'">Volver</button>
  </body>
  </html>
