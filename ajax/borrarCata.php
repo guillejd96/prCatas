da5f27b7ddbf36da0ceb97c953c0af1423c890c4
Guillermo <wasanga7@gmail.com>
2020-04-20 21:16:36 +0200

    Create borrarCata.php

---
+++ ajax/borrarCata.php
@@ -0,0 +1,37 @@
+<?php 
+
+	include '../config.php';
+
+	$idCata = $_GET['id'];
+
+	$sqlCervezas = "SELECT id FROM cerveza WHERE idCata=".$idCata;
+	$resCervezas = mysqli_query($conexion,$sqlCervezas);
+
+	while($c = mysqli_fetch_array($resCervezas)){
+		$idCerveza = $c[0];
+
+		$sqlBorrarOpiniones = "DELETE FROM opinion WHERE idCerveza=".$idCerveza;
+		mysqli_query($conexion,$sqlBorrarOpiniones);
+
+		$sqlBorrarCerveza = "DELETE FROM cerveza WHERE id=".$idCerveza;
+		mysqli_query($conexion,$sqlBorrarCerveza);
+	}
+
+	$sqlPersonaCata = "SELECT idPersona FROM persona_cata WHERE idCata=".$idCata;
+	$resPersonaCata = mysqli_query($conexion,$sqlPersonaCata);
+
+	while ($p = mysqli_fetch_array($resPersonaCata)) {
+		$idPersona = $p[0];
+
+		$sqlBorrarPersonaCata = "DELETE FROM persona_cata WHERE idPersona=".$idPersona." AND idCata=".$idCata.";";
+		mysqli_query($conexion,$sqlBorrarPersonaCata);
+
+		$sqlBorrarPersona = "DELETE FROM persona WHERE id=".$idPersona." AND idUsuario=NULL";
+		mysqli_query($conexion,$sqlBorrarPersona);
+	}
+
+	$sqlBorrarCata = "DELETE FROM cata WHERE id=".$idCata;
+	mysqli_query($conexion,$sqlBorrarCata);
+
+	echo "1";
+ ?>
