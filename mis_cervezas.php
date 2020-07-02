07b0f6a2503d71a15b2fe5972deb38e4de17a515
Guillermo <wasanga7@gmail.com>
2020-07-02 22:07:49 +0200

    Update mis_cervezas.php

--- mis_cervezas.php
+++ mis_cervezas.php
@@ -1,11 +1,14 @@
 <?php 
 	include 'config.php';
+	include 'menus.php';
 	session_start();
 	if(!isset($_SESSION["idUsuario"])){
 		header('Location: index.php');
 	}
 
-	$idPersona = $_SESSION["idUsuario"];
+	$idUsuario = $_SESSION["idUsuario"];
+
+	$idPersona = mysqli_query($conexion,"SELECT id FROM persona WHERE idUsuario=".$idUsuario)->fetch_row()[0];
 
 	$sql = "SELECT * FROM opinion WHERE idPersona=".$idPersona;
 	$resOpiniones = mysqli_query($conexion,$sql);
@@ -17,74 +20,80 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Mis cervezas</title>
 </head>
 <body>
-	<h1>Mis Cervezas</h1>
-	<h3>Aquí se muestran un listado de todas las valoraciones que has hecho</h3>
-	
-	<table>
-		<?php if(mysqli_num_rows($resOpiniones)>0){
-			echo "<tr>
-		 			<th>
-		 				<p>Cerveza</p>
-		 			</th>
-		 			<th>
-		 				<p>Aroma</p>
-		 			</th>
-		 			<th>
-		 				<p>Apariencia</p>
-		 			</th>
-		 			<th>
-		 				<p>Sabor</p>
-		 			</th>
-		 			<th>
-		 				<p>Cuerpo</p>
-		 			</th>
-		 			<th>
-		 				<p>Botellín</p>
-		 			</th>
-		 			<th>
-		 				<p>Media</p>
-		 			</th>
-		 		</tr>";
-		 		if($resOpiniones->num_rows>0){
+	<?php echo banner(); ?>
+	<?php echo arriba(); ?>
+	<?php echo izquierda(); ?>
+	<div class="main">
+		<h1>Mis Cervezas</h1>
+		<h3>Aquí se muestran un listado de todas las valoraciones que has hecho</h3>
+		
+		<table>
+			<?php if(mysqli_num_rows($resOpiniones)>0){
+				echo "<tr>
+			 			<th>
+			 				<p>Cerveza</p>
+			 			</th>
+			 			<th>
+			 				<p>Aroma</p>
+			 			</th>
+			 			<th>
+			 				<p>Apariencia</p>
+			 			</th>
+			 			<th>
+			 				<p>Sabor</p>
+			 			</th>
+			 			<th>
+			 				<p>Cuerpo</p>
+			 			</th>
+			 			<th>
+			 				<p>Botellín</p>
+			 			</th>
+			 			<th>
+			 				<p>Media</p>
+			 			</th>
+			 		</tr>";
+			 		if($resOpiniones->num_rows>0){
 
- 				while($row = mysqli_fetch_assoc($resOpiniones)){
- 						$sqlCerveza = "SELECT nombre FROM cerveza WHERE id=".$row['idCerveza'].";";
-	 					$cerveza = mysqli_query($conexion,$sqlCerveza)->fetch_row()[0];
+	 				while($row = mysqli_fetch_assoc($resOpiniones)){
+	 						$sqlCerveza = "SELECT nombre FROM cerveza WHERE id=".$row['idCerveza'].";";
+		 					$cerveza = mysqli_query($conexion,$sqlCerveza)->fetch_row()[0];
 
-	 					echo "<tr>";
-	 					echo "<td><p>".$cerveza."</p></td>";
-	 					echo "<td><p>".$row['aroma']."</p></td>";
-	 					echo "<td><p>".$row['apariencia']."</p></td>";
-	 					echo "<td><p>".$row['sabor']."</p></td>";
-	 					echo "<td><p>".$row['cuerpo']."</p></td>";
-	 					if($row['botellin']!=NULL){
-	 						echo "<td><p>".$row['botellin']."</p></td>";
-	 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
-	 						echo "<td><p>".$media."</p></td>";
-	 					} else {
-	 						echo "<td><p>--</p></td>";
-	 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']) / 4;
-	 						echo "<td><p>".$media."</p></td>";
-	 					}
-	 					echo "<tr>";
- 				}
- 			} else {
- 				echo "<tr><td colspan='6'><p>No hay resultados disponibles</p></td></tr>";
- 			}
+		 					echo "<tr>";
+		 					echo "<td><p>".$cerveza."</p></td>";
+		 					echo "<td><p>".$row['aroma']."</p></td>";
+		 					echo "<td><p>".$row['apariencia']."</p></td>";
+		 					echo "<td><p>".$row['sabor']."</p></td>";
+		 					echo "<td><p>".$row['cuerpo']."</p></td>";
+		 					if($row['botellin']!=NULL){
+		 						echo "<td><p>".$row['botellin']."</p></td>";
+		 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
+		 						echo "<td><p>".$media."</p></td>";
+		 					} else {
+		 						echo "<td><p>--</p></td>";
+		 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']) / 4;
+		 						echo "<td><p>".$media."</p></td>";
+		 					}
+		 					echo "<tr>";
+	 				}
+	 			} else {
+	 				echo "<tr><td colspan='6'><p>No hay resultados disponibles</p></td></tr>";
+	 			}
 
-		} else {
-			echo "<tr><td>";
-			echo "<p>No has registrado ninguna cerveza</p>";
-			echo "</td></tr>";
-			echo "<tr><td>";
-			echo "<p>Para registrar tu primera cata pulsa <a href='nueva_cata.php'>aquí</a></p>";
-			echo "</td></tr>";
-		} 
-		?>
-	</table><br>
-	<button class="btn btn-link" onclick="location.href='user.php'">Volver</button>
+			} else {
+				echo "<tr><td>";
+				echo "<p>No has registrado ninguna cerveza</p>";
+				echo "</td></tr>";
+				echo "<tr><td>";
+				echo "<p>Para registrar tu primera cata pulsa <a href='nueva_cata.php'>aquí</a></p>";
+				echo "</td></tr>";
+			} 
+			?>
+		</table><br>
+	</div>
 </body>
 </html>
