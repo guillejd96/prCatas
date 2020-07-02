b11bdd4c664a4a47e4330522f795200d7b467265
Guillermo <wasanga7@gmail.com>
2020-07-02 22:07:22 +0200

    Create instrucciones.php

---
+++ instrucciones.php
@@ -0,0 +1,28 @@
+<?php 
+
+	include 'menus.php';
+	session_start();
+	if(!isset($_SESSION["idUsuario"])){
+		header('Location: index.php');
+	}
+
+ ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Instrucciones</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<?php echo banner(); ?>
+	<?php echo arriba(); ?>
+	<?php echo izquierda(); ?>
+	<div class="main">
+	</div>
+</body>
+</html>
