227694a7bd7e1afe7065a97aad39cdb20d62feeb
Guillermo <wasanga7@gmail.com>
2020-07-07 20:42:37 +0200

    Update mis_cervezas.php

--- mis_cervezas.php
+++ mis_cervezas.php
@@ -20,69 +20,61 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Mis cervezas</title>
+	<script>
+	</script>
 </head>
 <body>
 	<?php echo banner(); ?>
 	<?php echo arriba(); ?>
 	<?php echo izquierda(); ?>
 	<div class="main">
-		<h1>Mis Cervezas</h1>
-		<h3>Aquí se muestran un listado de todas las valoraciones que has hecho</h3>
-		
-		<table>
+		<table class="display" id="content">
 			<?php if(mysqli_num_rows($resOpiniones)>0){
 				echo "<tr>
 			 			<th>
 			 				<p>Cerveza</p>
 			 			</th>
-			 			<th>
+			 			<th title='Ordenar'>
 			 				<p>Aroma</p>
 			 			</th>
-			 			<th>
+			 			<th title='Ordenar'>
 			 				<p>Apariencia</p>
 			 			</th>
-			 			<th>
+			 			<th title='Ordenar'>
 			 				<p>Sabor</p>
 			 			</th>
-			 			<th>
+			 			<th title='Ordenar'>
 			 				<p>Cuerpo</p>
 			 			</th>
-			 			<th>
+			 			<th title='Ordenar'>
 			 				<p>Botellín</p>
 			 			</th>
-			 			<th>
+			 			<th title='Ordenar'>
 			 				<p>Media</p>
 			 			</th>
 			 		</tr>";
 			 		if($resOpiniones->num_rows>0){
-
-	 				while($row = mysqli_fetch_assoc($resOpiniones)){
+	 					while($row = mysqli_fetch_assoc($resOpiniones)){
 	 						$sqlCerveza = "SELECT nombre FROM cerveza WHERE id=".$row['idCerveza'].";";
 		 					$cerveza = mysqli_query($conexion,$sqlCerveza)->fetch_row()[0];
-
 		 					echo "<tr>";
 		 					echo "<td><p>".$cerveza."</p></td>";
 		 					echo "<td><p>".$row['aroma']."</p></td>";
 		 					echo "<td><p>".$row['apariencia']."</p></td>";
 		 					echo "<td><p>".$row['sabor']."</p></td>";
 		 					echo "<td><p>".$row['cuerpo']."</p></td>";
-		 					if($row['botellin']!=NULL){
-		 						echo "<td><p>".$row['botellin']."</p></td>";
-		 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
-		 						echo "<td><p>".$media."</p></td>";
-		 					} else {
-		 						echo "<td><p>--</p></td>";
-		 						$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']) / 4;
-		 						echo "<td><p>".$media."</p></td>";
-		 					}
-		 					echo "<tr>";
-	 				}
-	 			} else {
+		 					echo "<td><p>".$row['botellin']."</p></td>";
+		 					$media = ($row['cuerpo']+$row['aroma']+$row['apariencia']+$row['sabor']+$row['botellin']) / 5;
+		 					echo "<td><p>".$media."</p></td>";
+		 					echo "</tr>";
+	 					}
+	 				} else {
 	 				echo "<tr><td colspan='6'><p>No hay resultados disponibles</p></td></tr>";
-	 			}
+	 				}
 
 			} else {
 				echo "<tr><td>";
