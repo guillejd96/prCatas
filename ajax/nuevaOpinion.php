c5e37cf6b6e5d4af9b07792ce6dcc4247d081609
Guillermo <wasanga7@gmail.com>
2020-07-15 22:19:52 +0200

    Update nuevaOpinion.php

--- ajax/nuevaOpinion.php
+++ ajax/nuevaOpinion.php
@@ -1,19 +1,30 @@
 <?php 
 
-include '../config.php';
+	include '../config.php';
 
-$persona = $_GET['p'];
-$cerveza = $_GET['c'];
-$aroma = $_GET['ar'];
-$apariencia = $_GET['ap'];
-$sabor = $_GET['s'];
-$cuerpo = $_GET['cu'];
-$botellin = $_GET['b'];
+	$persona = $_POST['idP'];
+	$cerveza = $_POST['idC'];
+	$aroma = $_POST['ar'];
+	$apariencia = $_POST['ap'];
+	$sabor = $_POST['s'];
+	$cuerpo = $_POST['c'];
+	$botellin = $_POST['b'];
 
-$sql = "INSERT INTO opinion(idCerveza, idPersona, apariencia, aroma, sabor, cuerpo, botellin) VALUES (".$cerveza.",".$persona.",".$apariencia.",".$aroma.",".$sabor.",".$cuerpo.",".$botellin.");";
-if(mysqli_query($conexion,$sql)){
-	echo "1";
-}else {
-	echo "0";
-}
- ?>
+	$res = mysqli_query($conexion,"SELECT * FROM opinion WHERE idPersona=".$persona." AND idCerveza=".$cerveza);
+	if(mysqli_num_rows($res)>0){
+		$sql = "UPDATE opinion SET apariencia=".$apariencia.", aroma=".$aroma.", sabor=".$sabor.", cuerpo=".$cuerpo.", botellin=".$botellin." WHERE idPersona=".$persona." AND idCerveza=".$cerveza;
+		if(mysqli_query($conexion,$sql)){
+			echo "1";
+		}else {
+			echo "0";
+		}
+	}else {
+		$sql = "INSERT INTO opinion(idCerveza, idPersona, apariencia, aroma, sabor, cuerpo, botellin) VALUES (".$cerveza.",".$persona.",".$apariencia.",".$aroma.",".$sabor.",".$cuerpo.",".$botellin.");";
+		if(mysqli_query($conexion,$sql)){
+			echo "1";
+		}else {
+			echo "0";
+		}
+	}
+
+?>
