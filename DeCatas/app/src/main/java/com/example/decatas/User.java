e950a5425cb1e5277e4a1f11985b820044dce0a4
Guillermo <wasanga7@gmail.com>
2020-08-23 17:21:36 +0200

    Update User.java

--- DeCatas/app/src/main/java/com/example/decatas/User.java
+++ DeCatas/app/src/main/java/com/example/decatas/User.java
@@ -1,5 +1,6 @@
 package com.example.decatas;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -9,11 +10,19 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -37,6 +46,14 @@ public class User extends AppCompatActivity {
         textCervezas = findViewById(R.id.textCervezas);
         textAmigos = findViewById(R.id.textAmigos);
         requests = findViewById(R.id.requests);
+
+        boolean checked = false;
+        try {
+            if(checkRequests()) checked=true;
+            else checked=false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Map<String,String> params = new LinkedHashMap<>();
         params.put("id",idUsuario);
@@ -77,6 +94,13 @@ public class User extends AppCompatActivity {
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
+        }
+
+        if (checked) {
+            android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(User.this);
+            builder.setCancelable(true);
+            builder.setMessage(R.string.insert_requests_successfully);
+            builder.show();
         }
     }
 
@@ -133,6 +157,7 @@ public class User extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
                 Intent intent = new Intent(getApplicationContext(),Login.class);
+                intent.putExtra("autologin","0");
                 startActivity(intent);
             }
         });
@@ -140,5 +165,60 @@ public class User extends AppCompatActivity {
         builder.show();
     }
 
+    private boolean checkRequests() throws IOException {
+        boolean b = false;
+        if(Arrays.asList(fileList()).contains("requests.txt")){
+            b = true;
+            FileInputStream r = openFileInput("requests.txt");
+            InputStreamReader isr = new InputStreamReader(r);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            bufferedReader.close();
 
+            String[] requests = sb.toString().split("/");
+            for(String re : requests){
+                String[] infoRequest = re.split(";");
+                String file = infoRequest[0];
+                String infoData = infoRequest[1];
+                String data[] = infoData.split(",");
+                Map<String,String> params = new LinkedHashMap<>();
+                if(file.equals("nuevaCervezaIndividual.php")){
+                    params.put("id",idUsuario);
+                    params.put("n",data[0]);
+                    params.put("ar",data[1]);
+                    params.put("ap",data[2]);
+                    params.put("s",data[3]);
+                    params.put("c",data[4]);
+                    params.put("b",data[5]);
+                }
+
+                Connection con = new Connection(this,file,params);
+                while(con.getRes()==null);
+                String res = con.getRes();
+                if(res.equals("IOException") || !res.equals("1")){
+                    b=false;
+                } else {
+                    deleteRequestFromFile(requests,re);
+                }
+            }
+            if(b) deleteFile("requests.txt");
+        }
+        return b;
+    }
+
+    private void deleteRequestFromFile(String[] requests, String re) throws IOException {
+        String result= "";
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("requests.txt", Context.MODE_PRIVATE));
+        for(String r : requests){
+            if(!r.equals(re)){
+                result+=r;
+            }
+        }
+        outputStreamWriter.write(result);
+        outputStreamWriter.close();
+    }
 }
