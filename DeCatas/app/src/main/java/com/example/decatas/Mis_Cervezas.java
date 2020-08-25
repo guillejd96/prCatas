90fa2f9dd11f564a615a7837b0ce51393d3feb90
Guillermo <wasanga7@gmail.com>
2020-08-25 20:17:54 +0200

    Update Mis_Cervezas.java

--- DeCatas/app/src/main/java/com/example/decatas/Mis_Cervezas.java
+++ DeCatas/app/src/main/java/com/example/decatas/Mis_Cervezas.java
@@ -1,21 +1,26 @@
 package com.example.decatas;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -32,16 +37,33 @@ public class Mis_Cervezas extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         idUsuario = bundle.getString("id");
 
-        this.layout = (LinearLayout)findViewById(R.id.layout);
+        this.layout = findViewById(R.id.layout);
 
+        update();
+    }
+
+    public void update(){
+        layout.removeAllViews();
         Map<String,String> params = new LinkedHashMap<>();
         params.put("id",idUsuario);
+
+        TextView first_label = new TextView(this);
+        first_label.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.FILL_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+        ));
+        first_label.setGravity(Gravity.CENTER);
+        first_label.setPadding(10,10,10,10);
+        first_label.setTextSize(20);
+        first_label.setVisibility(View.VISIBLE);
+        first_label.setText(getResources().getString(R.string.label_delete_beer));
+        layout.addView(first_label);
+
         try {
             Connection con = new Connection(getApplicationContext(),"getCervezas.php",params);
             while(con.getRes()==null);
             String result = con.getRes();
             if(!result.equals("")){
-                layout.removeAllViews();
 
                 String[] cervezas = result.split(";");
 
@@ -49,18 +71,19 @@ public class Mis_Cervezas extends AppCompatActivity {
                     for(String c : cervezas){
                         String[] atribs = c.split(",");
                         if(atribs.length>1){
-                            String nombre = atribs[0];
-                            String aroma = atribs[1];
-                            String apariencia = atribs[2];
-                            String sabor = atribs[3];
-                            String cuerpo = atribs[4];
-                            String botellin = atribs[5];
+                            final String idCerveza = atribs[0];
+                            String nombre = atribs[1];
+                            String aroma = atribs[2];
+                            String apariencia = atribs[3];
+                            String sabor = atribs[4];
+                            String cuerpo = atribs[5];
+                            String botellin = atribs[6];
 
                             TextView tv = new TextView(this);
                             tv.setLayoutParams(new TableRow.LayoutParams(
-                                        TableRow.LayoutParams.FILL_PARENT,
-                                        TableRow.LayoutParams.WRAP_CONTENT
-                                ));
+                                    TableRow.LayoutParams.FILL_PARENT,
+                                    TableRow.LayoutParams.WRAP_CONTENT
+                            ));
                             tv.setTypeface(Typeface.DEFAULT,Typeface.BOLD);
                             tv.setGravity(Gravity.CENTER);
                             tv.setPadding(10,10,10,10);
@@ -68,7 +91,27 @@ public class Mis_Cervezas extends AppCompatActivity {
                             tv.setVisibility(View.VISIBLE);
                             tv.setText(nombre);
                             tv.setPaintFlags(tv.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+                            Map<String,String> p = new LinkedHashMap<>();
+                            p.put("id",idCerveza);
+                            Connection conn = new Connection(this,"getCataDesdeCerveza.php",p);
+                            while(conn.getRes()==null);
+                            String res = conn.getRes();
+                            if(res.equals("")){
+                                Delete d = new Delete(idCerveza);
+                                tv.setOnClickListener(d);
+                            }else {
+                                tv.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        android.app.AlertDialog.Builder builder = new AlertDialog.Builder(Mis_Cervezas.this);
+                                        builder.setCancelable(true);
+                                        builder.setMessage(R.string.cant_delete_beer);
+                                        builder.show();
+                                    }
+                                });
+                            }
                             layout.addView(tv);
+
 
                             tv = new TextView(this);
                             tv.setLayoutParams(new TableRow.LayoutParams(
@@ -159,6 +202,61 @@ public class Mis_Cervezas extends AppCompatActivity {
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
+        }
+    }
+
+    private class Delete implements View.OnClickListener {
+
+        public String idCerveza;
+
+        public Delete(String s){
+            this.idCerveza=s;
+        }
+
+        @Override
+        public void onClick(View v) {
+            android.app.AlertDialog.Builder builder = new AlertDialog.Builder(Mis_Cervezas.this);
+            builder.setCancelable(true);
+            builder.setMessage(getResources().getString(R.string.question_delete_beer));
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Map<String,String> params = new LinkedHashMap<>();
+                    params.put("id",idCerveza);
+                    try {
+                        Connection con = new Connection(Mis_Cervezas.this,"deleteCervezaCata.php",params);
+                        while(con.getRes()==null);
+                        String result = con.getRes();
+                        if(result.equals("IOException")){
+                            OutputStreamWriter outputStreamWriter = null;
+                            if(!Arrays.asList(fileList()).contains("requests.txt")) {
+                                new File(getFilesDir(), "requests.txt");
+                                outputStreamWriter = new OutputStreamWriter(openFileOutput("requests.txt", Context.MODE_PRIVATE));
+                            }else {
+                                outputStreamWriter = new OutputStreamWriter(openFileOutput("requests.txt", Context.MODE_APPEND));
+                            }
+                            outputStreamWriter.write("deleteCata.php;"+idCerveza+"/");
+                            outputStreamWriter.close();
+
+                            android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(Mis_Cervezas.this);
+                            builder.setCancelable(true);
+                            builder.setTitle(R.string.error_connecting);
+                            builder.setMessage(R.string.ioexception_message);
+                            builder.show();
+                        }else {
+                            update();
+                        }
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            builder.setNegativeButton(R.string.cancel, null);
+            builder.show();
         }
     }
 }
