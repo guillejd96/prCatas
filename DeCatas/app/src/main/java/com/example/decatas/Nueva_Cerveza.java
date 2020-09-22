7fdad5a9f4482811e96586cd6bb040a96d47584e
Guillermo <wasanga7@gmail.com>
2020-09-22 20:23:23 +0200

    Update Nueva_Cerveza.java

--- DeCatas/app/src/main/java/com/example/decatas/Nueva_Cerveza.java
+++ DeCatas/app/src/main/java/com/example/decatas/Nueva_Cerveza.java
@@ -45,9 +45,6 @@ public class Nueva_Cerveza extends AppCompatActivity {
     String idUsuario;
     EditText inputName,inputAroma,inputApariencia,inputSabor,inputCuerpo,inputBotellin;
     String name,aroma,apariencia,sabor,botellin,cuerpo;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 1;
-    public Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +61,6 @@ public class Nueva_Cerveza extends AppCompatActivity {
         inputCuerpo = (EditText)findViewById(R.id.inputName5);
         inputBotellin = (EditText)findViewById(R.id.inputName6);
 
-        btn = (Button)findViewById(R.id.button2);
-
-        btnTakePhotoClicker b = new btnTakePhotoClicker();
-        btn.setOnClickListener(b);
     }
 
     public void goToUser(View v){
@@ -187,69 +180,4 @@ public class Nueva_Cerveza extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),res,Toast.LENGTH_LONG).show();
         }
 ;    }
-
-    private static final int CAN_REQUEST = 1313;
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==CAN_REQUEST){
-            final Bitmap bm = (Bitmap) data.getExtras().get("data");
-
-            android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
-            builder.setCancelable(true);
-            builder.setMessage(R.string.question_save_photo);
-
-            ImageView img = new ImageView(this);
-            img.setLayoutParams(new TableRow.LayoutParams(
-                    TableRow.LayoutParams.FILL_PARENT,
-                    TableRow.LayoutParams.FILL_PARENT));
-            img.setImageBitmap(bm);
-            img.getLayoutParams().height=100;
-            img.getLayoutParams().width=500;
-
-            builder.setView(img);
-
-            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-
-                    //ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                    //bm.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                    //byte[] imagen = stream.toByteArray();
-                    //String encodedImage = Base64.encodeToString(imagen,Base64.DEFAULT);
-
-                    //Map<String,String> p = new LinkedHashMap<>();
-                    //p.put("img",encodedImage);
-                    ///p.put("idU",idUsuario);
-                    //p.put("idC",idCerveza);
-
-                    //try {
-                    //   Connection c = new Connection(getApplicationContext(),"uploadImg.php",p);
-                    //   while(c.getRes()==null);
-                    //   if(c.getRes().equals("1")){
-                    //       Toast.makeText(Valorar_Cerveza.this, "La imagen se pudo guardar", Toast.LENGTH_SHORT).show();
-                    //   }else {
-                    //       Toast.makeText(Valorar_Cerveza.this, "Error al guardar la imagen", Toast.LENGTH_SHORT).show();
-                    //    }
-                    //} catch (MalformedURLException e) {
-                    //    e.printStackTrace();
-                    //}
-
-                }
-            });
-            builder.setNegativeButton(R.string.cancel, null);
-            builder.show();
-        }
-    }
-
-    private class btnTakePhotoClicker implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,CAN_REQUEST);
-        }
-    }
 }
